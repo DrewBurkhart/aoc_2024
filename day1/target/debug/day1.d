26cbc13ad0525d5a0514a988a7d5bf7b2effcf33
Andrew Burkhart <DrewBurkhart@users.noreply.github.com>
2024-12-01 19:16:43 -0700

    Add files via upload

---
+++ day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+/home/angooba/dev/aoc/day1/target/debug/day1: /home/angooba/dev/aoc/day1/src/main.rs
---
+++ day1/target/debug/deps/day1-2391ba4ceabcb65b.d
@@ -0,0 +1,5 @@
+/home/angooba/dev/aoc/day1/target/debug/deps/day1-2391ba4ceabcb65b: src/main.rs
+
+/home/angooba/dev/aoc/day1/target/debug/deps/day1-2391ba4ceabcb65b.d: src/main.rs
+
+src/main.rs:
